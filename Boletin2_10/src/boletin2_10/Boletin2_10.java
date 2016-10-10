caf404c7a6e9dadd34edfd01f859b588d8f72070
Adrián Silva <adri4silva@gmail.com>
2016-10-10 08:57:53 +0200

    Cambios en ejercicio 10

--- Boletin2_10/src/boletin2_10/Boletin2_10.java
+++ Boletin2_10/src/boletin2_10/Boletin2_10.java
@@ -14,13 +14,15 @@ public class Boletin2_10 {
      */
     public static void main(String[] args) {
 
-        double n100, n20, n5, n1, total; //Declaracion de variables
+        double bille100, resto, bille20, bille5, total; //Declaracion de variables
         total = Double.parseDouble(JOptionPane.showInputDialog("Inserta el total:  "));  //Pedimos el total
-        n100 = total % 100; //Damos el numero de billetes de 100
-        n20 = n100 % 20; //Damos el numero de billetes de 20
-        n5 = n20 % 5; //Damos el numero de billetes de 5
-        n1 = n5 % 1; //Damos el numero de monedas de 1
-        JOptionPane.showMessageDialog(null, "La cantidad de billetes de cien es: " + n100 + "la cantidad de billetes de veinte es: " + n20 + "la cantidad de billetes de cinco es: " + n5 + "la cantidad de monedas de uno es: " + n1 ); // Mostramos por pantalla cada cantidad
+        bille100 = total/100; 
+        resto = total - bille100*100;
+        bille20 = resto/20;
+        resto = resto - bille20*20;
+        bille5 = resto/5;
+        resto =resto - bille5*5;
+        JOptionPane.showMessageDialog(null, "La cantidad de billetes de cien es: " + bille100 + "la cantidad de billetes de veinte es: " + bille20 + "la cantidad de billetes de cinco es: " + bille5 + "la cantidad de monedas de uno es: " + resto ); // Mostramos por pantalla cada cantidad
 
     }
 
